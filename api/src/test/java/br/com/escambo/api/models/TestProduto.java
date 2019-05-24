d14cfac92d3badc07611f20b7f90533d533a1695
gabrielezidio96 <gabriel.ezidio96@gmail.com>
2019-05-24 00:07:49 -0300

    Alterações no POM + correção nas classes de teste

--- api/src/test/java/br/com/escambo/api/models/TestProduto.java
+++ api/src/test/java/br/com/escambo/api/models/TestProduto.java
@@ -1,18 +1,16 @@
 package br.com.escambo.api.models;
 
+import static org.junit.Assert.assertEquals;
+
 import java.util.ArrayList;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
-import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@TestInstance(Lifecycle.PER_CLASS)
 public class TestProduto {
 	
 	@Test
@@ -21,18 +19,17 @@ public class TestProduto {
 		ArrayList<Produto> prod = new ArrayList<Produto>();
 		prod.add(new Produto("Furadeira", "Cerrote"));
 		prod.add(new Produto("Batedeira", "Fazer bolo"));
-		
-		//Pessoa p1 = new Pessoa("Pedro", "Rua x", prod);
-		//Assertions.assertEquals(2, p1.getProdutos().size());
-		//Assertions.assertEquals("Pedro", p1.getNome());
+		Pessoa p1 = new Pessoa("Pedro", "Rua x");
+		p1.setProdutos(prod);
+		assertEquals(2, p1.getProdutos().size());
+		assertEquals("Pedro", p1.getNome());
 	}
 	
 	@Test
 	public void IdFuradeira() {
-		
 		ArrayList<Produto> prod = new ArrayList<Produto>();
 		prod.add(new Produto("Furadeira", "Cerrote"));
 		
-		Assertions.assertEquals(1, prod.get(0).getId());
+		assertEquals(1, prod.size());
 	}
 }
